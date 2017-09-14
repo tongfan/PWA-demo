58bd1386533fbc302b18796d93d05b23b4a46323
ryan <duhualei456@foxmail.com>
2017-09-14 17:52:46 +0800

    pwa

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 var dataCacheName = 'PWA-demo-data-v0.0.1';
 var cacheName = 'PWA-demo-v0.0.1';
 var filesToCache = [
-    '/',
-    '/index.html',
-    '/scripts/app.js',
-    '/styles/main.css',
-    '/images/ic_refresh_white_24px.svg'
+    '/PWA-demo/',
+    '/PWA-demo/index.html',
+    '/PWA-demo/scripts/app.js',
+    '/PWA-demo/styles/main.css',
+    '/PWA-demo/images/ic_refresh_white_24px.svg'
 ];
 
 // on install
