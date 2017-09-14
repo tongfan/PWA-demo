dbba894a12fad77cb64a7bf009f7048c2a3cf7c4
ryan <duhualei456@foxmail.com>
2017-09-14 15:13:18 +0800

    pwa

---
+++ scripts/app.js
@@ -0,0 +1,74 @@
+(function () {
+    'use strict';
+
+    var app = {
+        list: null,
+        itemTemplate: document.querySelector('.item-template'),
+        container: document.querySelector('.main')
+    };
+
+    var defaultData = [
+        {
+            "name": "PWA-demo",
+            "full_name": "tongfan/PWA-demo"
+        }
+    ];
+
+    app.getData = function () {
+        var url = 'https://api.github.com/users/tongfan/starred';
+
+        if ('caches' in window) {
+            caches.match(url).then(function (response) {
+                if (response) {
+                    response.json().then(function updateFromCache(results) {
+                        app.list = results;
+                        app.updateUi(results);
+                    });
+                }
+            });
+        }
+
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    var results = JSON.parse(request.response);
+                    app.list = results;
+                    app.updateUi(results);
+                }
+            } else {
+                if (!app.list) {
+                    app.list = defaultData;
+                    app.updateUi(defaultData);
+                }
+            }
+        };
+        request.open('GET', url);
+        request.send();
+    };
+
+    app.updateUi = function (datas) {
+        var ul = document.createElement('ul');
+        ul.className = 'list';
+        datas.forEach(function (data) {
+            var item = app.itemTemplate.cloneNode(true);
+            item.classList.remove('item-template');
+            item.innerHTML = '<a href="https://github.com/' + data.full_name + '">' + data.name + '</a>';
+            ul.appendChild(item);
+        });
+        app.container.innerHTML = ul.outerHTML;
+    };
+
+    app.getData();
+
+    document.getElementById('butRefresh').addEventListener('click', function () {
+        app.getData();
+    });
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./service-worker.js').then(function () {
+            console.log('Service Worker Registered');
+        });
+    }
+
+})();
---
+++ service-worker.js
@@ -0,0 +1,55 @@
+var dataCacheName = 'PWA-demo-data-v0.0.1';
+var cacheName = 'PWA-demo-v0.0.1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/styles/normalize.css',
+    '/styles/main.css',
+    '/images/ic_refresh_white_24px.svg'
+];
+
+// on install
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// on fetch
+self.addEventListener('fetch', function (event) {
+    var dataUrl = 'https://api.github.com/';
+    if (event.request.url.indexOf(dataUrl) > -1) {
+        event.respondWith(
+            caches.open(dataCacheName).then(function (cache) {
+                return fetch(event.request).then(function (response) {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    // 激活worker
+    return self.clients.claim();
+});
